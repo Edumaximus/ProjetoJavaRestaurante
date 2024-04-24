17ca44a1d237c7d60d43371a3ad293f30adff8b8
Juliomichaliszen <163341158+Juliomichaliszen@users.noreply.github.com>
2024-04-24 20:12:09 -0300

    Update Cardapio.java
    Adição da lista e Construtor

--- Cardapio.java
+++ Cardapio.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cardapio{
 
-    public int idPrato;
-    public String prato;
-    public Double preco;
-    public int quantidade;
+    public int idPrato; // identificacao do prato
+    public String prato; // Nome do prato
+    public Double preco; // Preco do prato
+    public int quantidade; // Quantidade disponivel
+
+    private list<Produto> produtos; // Lista de produtos
+
+    //Construtor da classe
+    public Cardapio(){
+        this.produtos = new ArrayList<>();
+    }
 
 }
