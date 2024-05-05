72788fdf838605eba6be64f56be42cb4576b7cad
Juliomichaliszen <163341158+Juliomichaliszen@users.noreply.github.com>
2024-05-05 19:07:39 -0300

    Update Cardapio.java
    Adição de métodos do cardápio.

--- models/Cardapio.java
+++ models/Cardapio.java
@@ -7,4 +7,20 @@ public class Cardapio{
     public int idPrato; // identificacao do prato
     public String prato; // Nome do prato
     public Double preco; // Preco do prato
+
+public Cardapio(int idPrato, String prato, Double preco){
+    this.idPrato = idPrato;
+        this.prato = prato;
+            this.preco = preco;
+}
+    
+    public int getIdPrato(){
+        return this.idPrato;
+    }
+        public String getPrato(){
+            return this.prato;
+        }
+            public Double getPreco(){
+                return this.preco;
+            }
 }
